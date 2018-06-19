782a0a2c6ceb4a221b5acf8d6c1a942d300c5889
zhengshuai <zhengshuai@xiaomalixing.com>
2018-06-19 19:59:53 +0800

    广告效果

---
+++ app/src/main/java/com/liger/practice/view/AdView.java
@@ -0,0 +1,66 @@
+package com.liger.practice.view;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * @author zs
+ * @date 2018/6/19 0019.
+ */
+@SuppressLint("AppCompatCustomView")
+public class AdView extends ImageView {
+
+    private int itemHeight;
+    /**
+     * 初始显示比率
+     */
+    private float rate = 1;
+
+    public AdView(Context context) {
+        super(context);
+    }
+
+    public AdView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AdView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public AdView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        itemHeight = h;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Drawable drawable = getDrawable();
+        if (drawable == null) {
+            return;
+        }
+        int w = getWidth();
+        int h = getWidth() / drawable.getIntrinsicWidth() * drawable.getIntrinsicHeight();
+        //设置图片显示的范围
+        drawable.setBounds(0, 0, w, h);
+        //图片可以移动的最大距离
+        int maxY = h - itemHeight;
+        canvas.save();
+        canvas.translate(0, -rate * maxY);
+        super.onDraw(canvas);
+        canvas.restore();
+    }
+}
